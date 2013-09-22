b75ce78c4ebee0dbc6edfdbbccd289e5a6a6a177
Khandar William <kuro.senchou@gmail.com>
2013-09-22 20:05:37 +0700

    Ember starter kit

--- front/ember/js/app.js
+++ front/ember/js/app.js
@@ -1,2 +1,11 @@
-/*global Ember */
-window.Todos = Ember.Application.create();
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
