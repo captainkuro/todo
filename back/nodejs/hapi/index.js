d833dc4140e0091702494a1fe2dd083509c0e418
Khandar William <khandar.william@gdpventure.com>
2013-12-04 11:11:04 +0700

    prepare node.js implementations

---
+++ back/nodejs/hapi/index.js
@@ -0,0 +1,22 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = Hapi.createServer('localhost', 8000);
+
+// Define the route
+var hello = {
+    handler: function (request) {
+
+        request.reply({ greeting: 'hello world' });
+    }
+};
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/hello',
+    config: hello
+});
+
+// Start the server
+server.start();
